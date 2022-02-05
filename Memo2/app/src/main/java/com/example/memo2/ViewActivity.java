03f56e46eb70e8f343925ef029cfc905c3f3f301
Booooj <90837337+Booooj@users.noreply.github.com>
2022-02-05 22:30:41 +0900

    Update ViewActivity.java

--- Memo2/app/src/main/java/com/example/memo2/ViewActivity.java
+++ Memo2/app/src/main/java/com/example/memo2/ViewActivity.java
@@ -7,7 +7,7 @@ public class ViewActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_view);
+        setContentView(R.layout.a_view);
 
        
     }
