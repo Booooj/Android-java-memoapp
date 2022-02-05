94d91f1c58b2d0e94752f2895993f0a75b301b66
Booooj <90837337+Booooj@users.noreply.github.com>
2022-02-05 22:22:36 +0900

    Update User.java

--- Memo2/app/src/main/java/com/example/memo2/database/User.java
+++ Memo2/app/src/main/java/com/example/memo2/database/User.java
@@ -2,9 +2,9 @@ package com.example.memo.database;
 
 
     public static final class memo implements BaseColumns{
-        public static final String TABLE_NAME ="user";
+        public static final String TABLE_NAME ="memo";
         public static final String COL_ID ="id";
-        public static final String COL_NAME ="name";
+        public static final String COL_CONTENT ="content";
 
     }
 }
