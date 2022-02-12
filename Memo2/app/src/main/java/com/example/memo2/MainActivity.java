cab9d80f8856601ccc7c3407cea19804e2390054
Booooj <90837337+Booooj@users.noreply.github.com>
2022-02-13 08:02:09 +0900

    Update MainActivity.java

--- Memo2/app/src/main/java/com/example/memo2/MainActivity.java
+++ Memo2/app/src/main/java/com/example/memo2/MainActivity.java
@@ -13,5 +13,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    
+       Button goButton = findViewById(R.id.go_button);
+
+        goButton.setOnClickListener(v -> {
+            Intent intent = new Intent(getApplication(), Post.class);
+            startActivity(intent);
+        });
+}
+      private void show() {
+     
+      SQLiteDatabase db = dbHelper.getWritableDatabase();
+      Cursor cursor = db.query(MemoContract.Memo.TABLE,
+                new String[]{MemoContract.Memo._ID, MemoContract.Memo.COL_CONTENT},
+                null, null, null, null, null);
+      }
+      cursor.close();
+       db.close();
+    }
+
+  public void delete(View view) {
+         
+        db.close();
+        show();
     }
 }
