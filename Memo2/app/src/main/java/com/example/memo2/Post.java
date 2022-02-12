5f65ccc6a3ec494935756b9759cb37e23ba9d832
Booooj <90837337+Booooj@users.noreply.github.com>
2022-02-13 08:12:46 +0900

    Update Post.java

--- Memo2/app/src/main/java/com/example/memo2/Post.java
+++ Memo2/app/src/main/java/com/example/memo2/Post.java
@@ -1,6 +1,22 @@
+package com.example.memo2;
 
-
+import androidx.appcompat.app.AppCompatActivity;
 
 public class Post extends AppCompatActivity {
   
+  @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.post_activity);
+      
+       Button btn_Back = findViewById(R.id.btn_back);
+        btn_Back.setOnClickListener(v -> {
+            Intent intent = new Intent(getApplication(), MainActivity.class);
+            startActivity(intent);
+          
+           SQLiteDatabase db = mHelper.getWritableDatabase();
+                            ContentValues values = new ContentValues();
+                            values.put(MemoContract.Memo.COL_CONTENT, content);
+          
+             db.close();
 }
