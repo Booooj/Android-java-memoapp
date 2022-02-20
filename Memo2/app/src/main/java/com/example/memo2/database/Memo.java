3c02951dcad33de8b1b72c385c88ee6e0308d31e
Booooj <90837337+Booooj@users.noreply.github.com>
2022-02-20 10:13:22 +0900

    Update and rename MemoDBHelper.java to Memo.java

---
+++ Memo2/app/src/main/java/com/example/memo2/database/Memo.java
@@ -0,0 +1,14 @@
+package com.example.memo2.database;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity
+public class Memo {
+    @PrimaryKey
+    public int uid;
+
+    @ColumnInfo(content = "content")
+    public String content;
+}
