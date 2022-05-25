5d658c58184f3e8e0361e415e781e7e488cf2bdc
Booooj <90837337+Booooj@users.noreply.github.com>
2022-05-25 10:56:38 +0900

    Update and rename Post.java to DBHelper.java

---
+++ Memo2/app/src/main/java/com/example/memo2/DBHelper.java
@@ -0,0 +1,24 @@
+package com.example.memo2;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DBHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "memo.db";
+    private static final int DATABASE_VERSION =1;
+
+    public DBHelper(Context context){
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+   
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
+}
