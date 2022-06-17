e86ac3f6269fa0184200971aa52d44ff35c64f93
Booooj <90837337+Booooj@users.noreply.github.com>
2022-06-17 18:12:25 +0900

    Update DBHelper.java

--- Memo2/app/src/main/java/com/example/memo2/DBHelper.java
+++ Memo2/app/src/main/java/com/example/memo2/DBHelper.java
@@ -6,21 +6,23 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class DBHelper extends SQLiteOpenHelper {
 
-    private static final String DATABASE_NAME = "memo.db";
+    private static final String DATABASE_NAME = "note.db";
     private static final int DATABASE_VERSION =1;
 
-    public DBHelper(Context context){
+
+    DBHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
-  @Override
+
+    @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL("CREATE TABLE notes (" +
-                "id INTEGER PRIMARY KEY, content TEXT)");
+                "id INTEGER PRIMARY KEY, note TEXT)");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+        db.execSQL("DROP TABLE IF EXISTS notes");
+        onCreate(db);
     }
-
 }
